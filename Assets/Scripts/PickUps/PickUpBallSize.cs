876d854398a50b007220b486b016c9733f9e75e2
DESKTOP-CB0EGJG\Nadya <nr@redtower.by>
2021-05-27 17:13:57 +0300

    Add prefabs

---
+++ Assets/Scripts/PickUps/PickUpBallSize.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using UnityEngine;
+
+public class PickUpBallSize : BasePickUp
+{
+    [SerializeField] private float sizeFactor = 1.25f;
+
+    public static event Action<PickUpBallSize> OnPickUpBallSizeCollected;
+
+    public float SizeFactor
+    {
+        get => sizeFactor;
+    }
+
+    protected override void ApplyEffect()
+    {
+        OnPickUpBallSizeCollected?.Invoke(this);
+    }
+}
